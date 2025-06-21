9224f8605869c313a7026673a3556e4645e89fcb
Obomonied <obomonied@gmail.com>
2025-06-21 12:20:05 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🎬 Welcome to MovieDrop</h1>
+      <p>Download the latest foreign and African movies.</p>
+    </div>
+  );
+}
+
+export default App;
